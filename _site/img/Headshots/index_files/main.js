d993f898b212c5e101dd5a19a2c8ac59c92385c7
Géraud MATHE <mail@geraudmathe.com>
2013-08-30 09:37:24 +0100

    jekyll setup

---
+++ _site/img/Headshots/index_files/main.js
@@ -0,0 +1,86 @@
+
+// $('#adam-portrait').mouseenter(function() {
+//   $('#adam-profile').slideDown("slow");
+// });
+// $('#adam-profile .portrait').mouseleave(function() {
+//   $('#adam-profile').fadeOut("slow");
+// });
+
+$('#adam-portrait').hover(
+  function(){$('#adam-profile').slideDown("slow")},
+  function(){$('#adam-profile').slideUp(150)}
+);
+
+
+$('#andrea-portrait').hover(
+  function(){$('#andrea-profile').slideDown("slow")},
+  function(){$('#andrea-profile').slideUp(150)}
+);
+
+$('#ankoor-portrait').hover(
+  function(){$('#ankoor-profile').slideDown("slow")},
+  function(){$('#ankoor-profile').slideUp(150)}
+);
+
+$('#anna-portrait').hover(
+  function(){$('#anna-profile').slideDown("slow")},
+  function(){$('#anna-profile').slideUp(150)}
+);
+
+$('#britney-portrait').hover(
+  function(){$('#britney-profile').slideDown("slow")},
+  function(){$('#britney-profile').slideUp(150)}
+);
+
+$('#cj-portrait').hover(
+  function(){$('#cj-profile').slideDown("slow")},
+  function(){$('#cj-profile').slideUp(150)}
+);
+
+$('#emile-portrait').hover(
+  function(){$('#emile-profile').slideDown("slow")},
+  function(){$('#emile-profile').slideUp(150)}
+);
+
+$('#jamese-portrait').hover(
+  function(){$('#jamese-profile').slideDown("slow")},
+  function(){$('#jamese-profile').slideUp(150)}
+);
+
+$('#jamesp-portrait').hover(
+  function(){$('#jamesp-profile').slideDown("slow")},
+  function(){$('#jamesp-profile').slideUp(150)}
+);
+
+$('#jarkyn-portrait').hover(
+  function(){$('#jarkyn-profile').slideDown("slow")},
+  function(){$('#jarkyn-profile').slideUp(150)}
+);
+
+$('#julien-portrait').hover(
+  function(){$('#julien-profile').slideDown("slow")},
+  function(){$('#julien-profile').slideUp(150)}
+);
+
+$('#luke-portrait').hover(
+  function(){$('#luke-profile').slideDown("slow")},
+  function(){$('#luke-profile').slideUp(150)}
+);
+
+$('#salman-portrait').hover(
+  function(){$('#salman-profile').slideDown("slow")},
+  function(){$('#salman-profile').slideUp(150)}
+);
+
+$('#stewart-portrait').hover(
+  function(){$('#stewart-profile').slideDown("slow")},
+  function(){$('#stewart-profile').slideUp(150)}
+);
+
+$('#will-portrait').hover(
+  function(){$('#will-profile').slideDown("slow")},
+  function(){$('#will-profile').slideUp(150)}
+);
+
+  // function(){$('#adam-profile').css('display', 'block')},
+  // function(){$('#adam-profile').css('display', 'none')}
